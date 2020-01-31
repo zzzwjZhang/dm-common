f6f0b1f0ce242a90b9da84e8dc193bb09913241c
zzzwjZhang <564555197@qq.com>
2020-01-31 12:25:51 +0800

    Initial commit

---
+++ dm-common-client/src/main/java/cn/dm/DmCommonClientApplication.java
@@ -0,0 +1,13 @@
+package cn.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmCommonClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmCommonClientApplication.class, args);
+    }
+
+}
